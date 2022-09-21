d01d7fcdb48d588ca065c267165b292f701715fe
Brandon Antonio Alvarez Aragon <73765318+Kratos2111@users.noreply.github.com>
2022-09-21 01:24:56 -0600

    Laboratorio listo

--- Laboratorio AED version 2.0/Ejercicio2.Designer.cs
+++ Laboratorio AED version 2.0/Ejercicio2.Designer.cs
@@ -255,6 +255,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(599, 381);
             this.Controls.Add(this.radioButton1);
             this.Controls.Add(this.btnAgregar);
